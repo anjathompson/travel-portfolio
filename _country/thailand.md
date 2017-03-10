931e283f731656cbe2647997ae24e15ac2344c51
anjathompson <anja.thompson@gmail.com>
2017-03-10 15:16:49 +0800

    Added jQuery plugin

---
+++ _country/thailand.md
@@ -0,0 +1,5 @@
+---
+layout: country
+name: Traveling Thailand
+top: Top 10 sites to see in Thailand
+---
---
+++ _site/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+// jQuery goes here
+
+
+  $('.portfolio-images').isotope({
+
+
+  // options
+
+  itemSelector: '.portfolio-image',
+  layoutMode: 'fitRows'
+  });
+
+
+});

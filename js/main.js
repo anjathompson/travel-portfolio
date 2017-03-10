29a5fafbcf27fbb38e6deb82be831848d1ede75b
anjathompson <anja.thompson@gmail.com>
2017-03-10 21:46:41 +0800

    adding changes to thailand site

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@ $(document).ready(function() {
 
 // jQuery goes here
 
-
   $('.portfolio-images').isotope({
-
 
   // options
 
   itemSelector: '.portfolio-image'
   });
 
+});
+
+$(document).ready(function() {
+
+// jQuery goes here
+
+  $('.thailand-images').isotope({
+
+  // options
+
+  itemSelector: '.thailand-image'
+  });
 
 });

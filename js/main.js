7e1a7c37a12dfdd529dd2ef9c78fdfd2425f8d9e
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2017-03-11 19:16:37 +0800

    styled some stuff on the homepage

--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
 $(document).ready(function() {
 
-// jQuery goes here
-
   $('.portfolio-images').isotope({
-
-  // options
-
-  itemSelector: '.portfolio-image'
+    // options
+    itemSelector: '.portfolio-image'
   });
 
-});
-
-$(document).ready(function() {
-
-// jQuery goes here
-
   $('.thailand-images').isotope({
+    // options
+    itemSelector: '.thailand-image'
+  });
 
-  // options
+  // Get height to top from latest section
+  var heightToTop = $('h2').offset().top;
 
-  itemSelector: '.thailand-image'
+  $(window).scroll(function() {
+    // Trigger class when scrolled past latest section
+    if ($(window).scrollTop() > 50) {
+      $('header').addClass('scrolled');
+    } else {
+      $('header').removeClass('scrolled');
+    }
+
+    if ($(window).scrollTop() > heightToTop){
+      $('header').addClass('show-logo');
+    } else {
+      $('header').removeClass('show-logo');
+    }
+
+  });
+
+  // Animate scrolling
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 
 });
